be52013969ece6976b037994cc5a112614afa16a
Aleksandr Slyvka <alexandrslivka@gmail.com>
2023-03-02 18:46:31 +0100

    Did the first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+startBtn.addEventListener('click', () => {
+  timerEl = setInterval(() => {
+    startBtn.disabled = true;
+    bodyEl.style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  clearInterval(timerEl);
+});
